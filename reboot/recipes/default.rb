2f6e56cac107eea4d0a02c1adcd7ff61923c7105
Krishna Vudumula <RLE0401@rlpl.com>
2016-11-10 14:33:35 +0530

    this is my commit for reboot

--- reboot/recipes/default.rb
+++ reboot/recipes/default.rb
@@ -10,18 +10,46 @@
 #  command "/usr/bin/tput smso;/bin/echo 'This system needs to be REBOOTED';/usr/bin/tput rmso;sudo init 6" 
 #end
 
-#reboot 'needs_reboot' do
-#  action :request_reboot
-#  reason 'This system needs to be REBOOTED'
-#  command '/usr/bin/tput smso; /bin/echo 'This system needs to be REBOOTED'; /usr/bin/tput rmso'  
-#  delay_mins 1
+#execute 'needs_reboot' do
+  #action :request_reboot
+  #reason 'This system needs to be REBOOTED'
+#command '/bin/tput smso > /dev/"[pt]t[ys][0-9]"; /bin/wall hello-world > /dev/[pt]t[ys][0-9]; /bin/tput rmso > /dev/[pt]t[ys][0-9]'
+#  action :run  
+  #delay_mins 1
+  #command 'sudo init 6' 
 #end
 
-#execute 'now' do
-  #action :request_reboot
-  #reason 'Cannot continue Chef run without a reboot.'
-  #delay_mins 2
+#execute 'request_reboot' do
+#  action :request_reboot
+#  reason 'Cannot continue Chef run without a reboot.'
+#  delay_mins 2
 #end
-execute "sudo init 6" do
-  command '/usr/bin/tput smso&&/bin/echo "This system needs to be REBOOTED"&&/usr/bin/tput rmso'
+#execute "sudo init 6" do
+#  command '/usr/bin/tput smso&&/bin/echo "This system needs to be REBOOTED"&&/usr/bin/tput rmso'
+#end
+#bash 'needs_reboot' do
+#  code <<-EOH
+#    /bin/tput smso
+#    /bin/echo 'This system needs to be REBOOTED'
+#    /bin/tput rmso    
+#    EOH
+#end
+#bash 'needs_reboot' do
+#  code <<-EOH
+ # /bin/echo 'This system needs to be REBOOTED'
+#  EOH
+#end
+#execute 'needs_reboot' do
+  #action :request_reboot
+  #reason 'This system needs to be REBOOTED'
+#  command '/bin/tput smso > /dev/"[pt]t[ys][0-9]"; /bin/wall "This system needs to be REBOOTED" > /dev/[pt]t[ys][0-9]; /bin/tput rmso > /dev/[pt]t[ys][0-9]'  
+#end
+execute 'needs_reboot' do
+	command '/bin/tput smso > /dev/"[pt]t[ys][0-9]"; /bin/echo "This system needs to be REBOOTED" > /dev/[pt]t[ys][0-9]; /bin/tput rmso > /dev/[pt]t[ys][0-9]'
 end
+reboot 'requires_reboot' do
+	action :request_reboot
+	reason 'This system needs to be REBOOTED'
+	delay_mins 1
+end
+
